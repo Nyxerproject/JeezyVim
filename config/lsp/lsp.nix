bfbcfc7b78bb382af28b40379bedee48639e3c26
Nyxerproject <82858481+Nyxerproject@users.noreply.github.com>
2024-10-24 18:24:33 -0600

    Update lsp.nix

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -2,26 +2,22 @@
   plugins.lsp = {
     enable = pkgs.lib.mkDefault true;
     servers = {
+      markdown_oxide.enable = pkgs.lib.mkDefault true;
+      clangd.enable = pkgs.lib.mkDefault true;
       bashls.enable = pkgs.lib.mkDefault true;
+      cmake.enable = pkgs.lib.mkDefualt true;
       dockerls.enable = pkgs.lib.mkDefault true;
       gopls.enable = pkgs.lib.mkDefault true;
       jsonls.enable = pkgs.lib.mkDefault true;
-      marksman.enable = pkgs.lib.mkDefault true;
       nil_ls.enable = pkgs.lib.mkDefault true;
       pyright.enable = pkgs.lib.mkDefault true;
-      ts_ls.enable = pkgs.lib.mkDefault true;
       lua_ls.enable = pkgs.lib.mkDefault true;
       rust_analyzer = {
         enable = pkgs.lib.mkDefault true;
         installCargo = pkgs.lib.mkDefault false;
         installRustc = pkgs.lib.mkDefault false;
       };
-      tailwindcss.enable = pkgs.lib.mkDefault true;
-      typst_lsp.enable = pkgs.lib.mkDefault true;
-      cssls.enable = pkgs.lib.mkDefault true;
       html.enable = pkgs.lib.mkDefault true;
-      htmx.enable = pkgs.lib.mkDefault true;
-      solargraph.enable = pkgs.lib.mkDefault true;
       yamlls.enable = pkgs.lib.mkDefault true;
       taplo.enable = pkgs.lib.mkDefault true;
     };
